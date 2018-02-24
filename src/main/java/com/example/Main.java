f73b9cab196703f26fa6dd54f0adce965bb9931c
SOLaidOff <34584806+SOLaidOff@users.noreply.github.com>
2018-02-24 18:04:54 -0500

    Add rudimentary HTML framework

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -52,6 +52,18 @@ public class Main {
     String index() {
         return "index";
     }
+    
+    @RequestMapping("/ask")
+    String ask() {
+        return "ask";
+    }
+    
+    @RequestMapping("/question")
+    String question() {
+        // TODO: fill in model, using question ID
+        
+        return "question";
+    }
 
     // First step: Add new RequestMapping annotation for a URL, an associated method,
     // and an .html page under src/main/resources/templates with the returned name
@@ -65,16 +77,18 @@ public class Main {
     String db(Map<String, Object> model) {
         try (Connection connection = dataSource.getConnection()) {
             Statement stmt = connection.createStatement();
+            
             stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+            
             stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+            
             ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
-
             ArrayList<String> output = new ArrayList<String>();
             while (rs.next()) {
                 output.add("Read from DB: " + rs.getTimestamp("tick"));
             }
-
             model.put("records", output);
+
             return "db";
         } catch (Exception e) {
             model.put("message", e.getMessage());
